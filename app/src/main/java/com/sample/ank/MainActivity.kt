a703964ba471016b069a5f473024a88f8a1c795a
IchigoPres <163737491+IchigoPres@users.noreply.github.com>
2026-05-19 18:17:59 +0800

    Update MainActivity.kt

--- app/src/main/java/com/sample/ank/MainActivity.kt
+++ app/src/main/java/com/sample/ank/MainActivity.kt
@@ -1,1 +1,22 @@
+package com.sample.ank
 
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setPadding
+
+class MainActivity: AppCompatActivity {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main);
+	}
+	override fun onDestroy() {
+		super.onDestroy();
+	}
+	override fun onStart() {
+		super.onStart()
+	}
+	overridez fun onPause() {
+		super.onPause()
+	}
+}
